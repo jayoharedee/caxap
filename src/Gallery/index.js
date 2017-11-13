92d97fba7435d72f2c190d1476ce28acd4b4ae8d
Jordan Michael <jordanm@tuta.io>
2017-11-12 20:39:02 -0500

    added images and css changes for gallery and app component

--- src/Gallery/index.js
+++ src/Gallery/index.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import './CSS/Gallery.css';
 
-import cat from './Images/cat-800-480.jpg';
+import desktop from './Images/slide.jpg';
+import mobile from './Images/slide-mobile.jpg';
 
 class Gallery extends Component {
   render() {
     return (
       <div>
-        <div className='galley-section'>
-          <picture className='gallery-photo'>
-            <source media='(max-width: 799px)' srcset={cat} />
-            <source media='(min-width: 800px)' srcset={cat} />
-            <img src={cat} alt='Chris standing up holding his daughter Elva' />
+        <div className='gallery-section'>
+          <picture className='gallery-photo responsive-img'>
+            <source media='(max-width: 799px)' srcset={mobile} />
+            {/* <source media='(min-width: 800px)' srcset={} /> */}
+            <img src={desktop} alt='' />
           </picture>
         </div>
       </div>
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 import './CSS/Header.css';
-import brand from './Images/caxap.jpg';
+import brand from './Images/logo.jpg';
 
 class Header extends Component {
   render() {
     return (
       <div>
-        <header class="masthead clear">
-            <div class="centered">
-                <div class="site-branding">
+        <header className="masthead clear">
+            <div className="centered">
+                <div className="site-branding responsive-img">
                   <img src={brand} alt="logo" />
                 </div>
             </div>
         </header>
 
-        <div class="centered">
-            <section class="menu-section">
-                <nav id="single-nav" class="single-nav menu" role="navigation">
-                    <ul className="nav-parent">
+        <div className="centered">
+            <section className="menu-section">
+                <nav className="single-nav menu" role="navigation">
+                    <ul classNameName="nav-parent">
                         <li>
-                          <a href="#" className="menu-header">Photo Editing</a>
+                          <a href="#" classNameName="menu-header">Photo Editing</a>
                         </li>
                         <li>
-                          <a href="#" className="menu-header">Graphic Design</a>
+                          <a href="#" classNameName="menu-header">Graphic Design</a>
                         </li>
                     </ul>
                 </nav>
