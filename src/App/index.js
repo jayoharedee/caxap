fa95e5219deeb6e1818cf4dc1310c54ede5dbdac
Jordan Michael <jordanm@tuta.io>
2017-12-03 21:14:26 -0500

    updated travis for create-react-app builds

--- src/App/index.js
+++ src/App/index.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './CSS/App.css';
 
-import Header from '../Header';
-import Gallery from '../Gallery';
+// import Header from '../Header';
+// import Gallery from '../Gallery';
 
 class App extends Component {
   render() {
-    return (
-      <main className="main-area">
-        <div className="centered">
-          <Header />
-          <Gallery />
-        </div>
-      </main>
-    );
+    return <div>
+        <header className="v-header container">
+          <div className="fullscreen-video-wrap" />
+        </header>
+      </div>;
   }
 }
 
 export default App;
+
+
+// <main className="main-area">
+//   <div className="centered">
+//     <Header />
+//     <Gallery />
+//   </div>
+// </main>
